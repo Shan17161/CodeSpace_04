b35c26f72e84e5ef2bbc1daf417d4f83f72bc2fc
Shan17161 <137260427+Shan17161@users.noreply.github.com>
2024-12-20 16:47:55 +0000

    added signup and loader pages

---
+++ appwrite/src/components/AuthLayout.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+
+export default function Protected({children, authentication = true}){
+    const navigate = useNavigate();
+    const [loader, setLoader] = useState(true);
+    const authStatus = useSelector((state)=>state.auth.status)
+    useEffect(()=>{
+        
+    },[authStatus, navigate, authentication])
+    return(
+        <>
+        </>
+    )
+}
