f31d4d8f2be256a07244205e46cc55a761294b48
Shan17161 <137260427+Shan17161@users.noreply.github.com>
2024-12-21 17:54:04 +0000

    added RTE and PostForm components

--- appwrite/src/components/AuthLayout.jsx
+++ appwrite/src/components/AuthLayout.jsx
@@ -8,10 +8,14 @@ export default function Protected({children, authentication = true}){
     const [loader, setLoader] = useState(true);
     const authStatus = useSelector((state)=>state.auth.status)
     useEffect(()=>{
-        
+
+        if(authentication && authStatus !== authentication){
+            navigate('/login');
+        }
+        else if(!authentication && authStatus !== authentication){
+            navigate('/');
+        }
+        setLoader(false)
     },[authStatus, navigate, authentication])
-    return(
-        <>
-        </>
-    )
+    return loader ? <div>Loading...</div> : <>{children}</>
 }
--- appwrite/src/components/index.js
+++ appwrite/src/components/index.js
@@ -5,6 +5,8 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from './Button';
 import Input from './Input';
+import Select from './Select'
+import RTE from "./RTE";
 export {
 
     Header, 
@@ -14,5 +16,7 @@ export {
     LogoutBtn,
     Button,
     Input,
+    Select,
+    RTE
 
 }
